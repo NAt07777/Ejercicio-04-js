fdc63dc0bd6bb6804f6397b2a86a49c50a1ed0ec
Natalia_Infantino <116117191+NAt07777@users.noreply.github.com>
2023-03-20 11:56:42 -0300

    Add files via upload

---
+++ ejercicio-04.js
@@ -0,0 +1,23 @@
+let nombre = "Natalia";
+let apellido = "Infantino";
+
+let estudiante = `${nombre} ${apellido}`;
+
+let estudianteMayus = estudiante.toUpperCase();
+
+let estudianteMinus = estudiante.toLowerCase();
+
+let num_letras = estudiante.length;
+
+let inicial_nombre =  nombre.substring(0, 1);
+
+let fin_apellido = apellido.substring(8, 9);
+
+let sin_espacios = estudiante.trim.length;
+
+let contiene = estudiante.includes(nombre);
+
+
+
+
+
